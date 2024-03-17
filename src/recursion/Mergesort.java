a51ae15e9b0dff69d193cfbae424785a42eeb9bc
Anant Majhi <anmajhi7@gmail.com>
2024-03-17 11:48:52 +0530

    in place merge sort

--- src/recursion/Mergesort.java
+++ src/recursion/Mergesort.java
@@ -6,14 +6,59 @@ public class Mergesort {
 
     public static void main(String[] args) {
         int arr[] = {5, 4, 3, 2, 1};
-        mergeSortInPlace(arr, 0 , arr.length - 1);
+        mergeSortInPlace(arr, 0 , arr.length );
         int [] result = mergeSort(arr);
         System.out.println(Arrays.toString(result));
         System.out.println(Arrays.toString(arr));
     }
 
     private static void mergeSortInPlace(int[] arr, int start, int end) {
+        int mid = end + (start - end) / 2;
 
+        if(end - start == 1) return;
+
+        mergeSortInPlace(arr, start, mid);
+        mergeSortInPlace(arr,mid, end);
+
+        mergeInPlace(arr,start,mid,end);
+
+    }
+
+    private static void mergeInPlace(int[] arr, int s, int m, int e) {
+        int[] mix = new int[e - s];
+
+        int i = s;
+        int j = m;
+        int k = 0;
+
+        while (i < m && j < e) {
+            if (arr[i] < arr[j]) {
+                mix[k] = arr[i];
+                i++;
+            } else {
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        // it may be possible that one of the arrays is not complete
+        // copy the remaining elements
+        while (i < m) {
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        while (j < e) {
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        for (int l = 0; l < mix.length; l++) {
+            arr[s+l] = mix[l];
+        }
     }
 
     private static int[] mergeSort(int[] arr) {
